c2ab3670bb12eea3da5548e82a53c71c7d7e638b
王慧勇 <weifeng19911024@163.com>
2017-01-31 20:57:21 +0800

    first commit

--- TapTaps/ViewController.m
+++ TapTaps/ViewController.m
@@ -10,20 +10,68 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *singleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *doubleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tripleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *quadrupleLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.singleLabel.text = @"";
+    self.doubleLabel.text = @"";
+    self.tripleLabel.text = @"";
+    self.quadrupleLabel.text = @"";
+    
+    UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTap)];
+    singleTap.numberOfTapsRequired = 1;
+    singleTap.numberOfTouchesRequired = 1;
+    [self.view addGestureRecognizer:singleTap];
+    
+    UITapGestureRecognizer *doubleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(doubleTap)];
+    doubleTap.numberOfTapsRequired = 2;
+    doubleTap.numberOfTouchesRequired = 1;
+    [self.view addGestureRecognizer:doubleTap];
+    
+    [singleTap requireGestureRecognizerToFail:doubleTap];		// single 需要所有其他手势都失败
+    
+    UITapGestureRecognizer *tripleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tripleTap)];
+    tripleTap.numberOfTapsRequired = 3;
+    tripleTap.numberOfTouchesRequired = 1;
+    [self.view addGestureRecognizer:tripleTap];
+    
+    [doubleTap requireGestureRecognizerToFail:tripleTap];		// double 需要 triple 和 quadruple 失败
+    
+    UITapGestureRecognizer *quadrupleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(quadrupleTap)];
+    quadrupleTap.numberOfTapsRequired = 4;
+    quadrupleTap.numberOfTouchesRequired = 1;
+    [self.view addGestureRecognizer:quadrupleTap];
+    
+    [tripleTap requireGestureRecognizerToFail:quadrupleTap];		// triple 需要 quadruple 失败
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)singleTap {
+    self.singleLabel.text = @"Single Tap Detected";
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.singleLabel.text = @"";});
 }
 
+- (void)doubleTap {
+    self.doubleLabel.text = @"Double Tap Detected";
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.doubleLabel.text = @"";});
+}
+
+- (void)tripleTap {
+    self.tripleLabel.text = @"Triple Tap Detected";
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.tripleLabel.text = @"";});
+}
+
+- (void)quadrupleTap {
+    self.quadrupleLabel.text = @"Quadruple Tap Detected";
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{self.quadrupleLabel.text = @"";});
+}
 
 @end
